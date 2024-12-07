9620a44d6ae839bdab2145b19ef10001ebf5f29f
anton moiseev <moucee13@gmail.com>
2024-12-07 03:11:59 +0300

    documented LessonParser schema

--- eazyclass/scheduler/schemas.py
+++ eazyclass/scheduler/schemas.py
@@ -1,4 +1,5 @@
 from datetime import date, datetime
+from typing import Any
 
 from annotated_types import MaxLen
 from pydantic import BaseModel, Field, field_validator, model_validator
@@ -6,9 +7,6 @@ from typing_extensions import Annotated
 
 from scheduler.models import Subject, Classroom, Teacher
 
-# MAX_SUBJECT_TITLE_LENGTH = 255
-# MAX_CLASSROOM_TITLE_LENGTH = 10
-# MAX_TEACHER_FULLNAME_LENGTH = 64
 MAX_SUBJECT_TITLE_LENGTH = Subject._meta.get_field('title').max_length
 MAX_CLASSROOM_TITLE_LENGTH = Classroom._meta.get_field('title').max_length
 MAX_TEACHER_FULLNAME_LENGTH = Teacher._meta.get_field('full_name').max_length
@@ -19,6 +17,18 @@ SUBGROUP_DEFAULT_VALUE = 0
 
 
 class LessonParser(BaseModel):
+    """
+    Схема для парсинга данных об уроках
+
+    Attributes:
+        lesson_number (int): Номер урока (от 1 до 9). Обязательное поле.
+        subject_title (str): Название предмета. По умолчанию 'Не указано'.
+        classroom_title (str): Название аудитории. По умолчанию '(дист)'.
+        teacher_fullname (str): ФИО преподавателя. По умолчанию 'Не указано'.
+        subgroup (int): Подгруппа (0 или от 1 до 9). По умолчанию 0.
+        date (date): Дата проведения урока. По умолчанию None.
+        group_id (int): ID группы. По умолчанию None.
+    """
     lesson_number: int = Field(..., ge=1, le=9, description="Номер урока (от 1 до 9)")
     subject_title: str = Field(default=SUBJECT_DEFAULT_VALUE,
                                max_length=MAX_SUBJECT_TITLE_LENGTH,
@@ -35,7 +45,19 @@ class LessonParser(BaseModel):
 
     @field_validator("date", mode='before')
     @classmethod
-    def parse_date(cls, value):
+    def parse_date(cls, value: Any) -> date:
+        """
+        Валидатор для обработки поля `date`.
+
+        Args:
+            value (Any): Значение, переданное в поле `date`.
+
+        Returns:
+            date: Объект `date`, если значение валидно.
+
+        Raises:
+            ValueError: Если формат даты некорректен.
+        """
         if isinstance(value, date):
             return value
         if isinstance(value, str):
@@ -49,7 +71,17 @@ class LessonParser(BaseModel):
 
     @field_validator("subject_title", 'classroom_title', 'teacher_fullname', mode='before')
     @classmethod
-    def truncate_string(cls, value, info):
+    def truncate_string(cls, value: Any, info) -> str:
+        """
+        Валидатор для обрезки строковых значений полей содержащих max_length.
+
+        Args:
+            value (Any): Значение, переданное в поле.
+            info: Информация о поле.
+
+        Returns:
+            str: Обрезанная строка.
+        """
         if isinstance(value, str):
             # Ищем объект MaxLen в metadata чтобы получить max_length поля
             for meta in cls.model_fields[info.field_name].metadata:
@@ -61,7 +93,16 @@ class LessonParser(BaseModel):
 
     @model_validator(mode="before")
     @classmethod
-    def replace_empty_with_default(cls, values):
+    def replace_empty_with_default(cls, values: dict[str, Any]) -> dict[str, Any]:
+        """
+        Валидатор для замены пустых строк значениями по умолчанию
+
+        Args:
+            values (dict[str, Any]): Словарь входных данных.
+
+        Returns:
+            dict[str, Any]: Обновленный словарь данных.
+        """
         for field_name, value in values.items():
             if isinstance(value, str):
                 stripped_value = value.strip()
