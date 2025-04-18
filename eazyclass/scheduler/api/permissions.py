98da1522f404b0b8322fccccd9fcb86d2ef2f5be
anton moiseev <moucee13@gmail.com>
2025-04-18 07:03:31 +0300

    added IsAdminOrReadOnly permission

--- eazyclass/scheduler/api/permissions.py
+++ eazyclass/scheduler/api/permissions.py
@@ -4,3 +4,9 @@ from rest_framework.permissions import BasePermission
 class IsOwner(BasePermission):
     def has_object_permission(self, request, view, obj):
         return obj.user == request.user
+
+
+class IsAdminOrReadOnly(BasePermission):
+    """Разрешает чтение всем, изменение — только админам."""
+    def has_permission(self, request, view):
+        return request.method in ['GET', 'HEAD', 'OPTIONS'] or request.user.is_staff
