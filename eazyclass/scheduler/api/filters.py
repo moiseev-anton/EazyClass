1069b65f4ea0524fd183082cbe6e5dba6156d53b
anton moiseev <moucee13@gmail.com>
2025-04-18 07:00:31 +0300

    remade request param filters

--- eazyclass/scheduler/api/filters.py
+++ eazyclass/scheduler/api/filters.py
@@ -1,26 +1,44 @@
+from datetime import timedelta
+
 from django_filters import rest_framework as filters
 from rest_framework.exceptions import ValidationError
-from datetime import timedelta
-from scheduler.models import Lesson
+
+from scheduler.models import Lesson, Group, Faculty
 
 
-class LessonFilter(filters.FilterSet):
-    date_from = filters.DateFilter(field_name='period__date', lookup_expr='gte', required=True)
-    date_to = filters.DateFilter(field_name='period__date', lookup_expr='lte', required=True)
-    subgroup = filters.CharFilter(field_name='subgroup', lookup_expr='exact')
-    format = filters.CharFilter(method='ignore_format_filter')
-
-    class Meta:
-        model = Lesson
-        fields = ['date_from', 'date_to', 'subgroup']
+class BaseFilterSetWithFormat(filters.FilterSet):
+    format = filters.ChoiceFilter(
+        choices=[("full", "Full"), ("compact", "Compact")],
+        method="ignore_format_filter",
+        required=False,
+    )
 
     def ignore_format_filter(self, queryset, name, value):
         return queryset
 
+
+class LessonFilter(BaseFilterSetWithFormat):
+    LIST_ACTIVITIES = {"list", "by_group", "by_teacher", "get_me"}
+
+    date_from = filters.DateFilter(field_name="period__date", lookup_expr="gte")
+    date_to = filters.DateFilter(field_name="period__date", lookup_expr="lte")
+    subgroup = filters.CharFilter(field_name="subgroup", lookup_expr="exact")
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        view = self.request.parser_context.get("view")
+        if view and view.action in self.LIST_ACTIVITIES:
+            self.filters["date_from"].extra["required"] = True
+            self.filters["date_to"].extra["required"] = True
+
+    class Meta:
+        model = Lesson
+        fields = ["date_from", "date_to", "subgroup", "format"]
+
     def filter_queryset(self, queryset):
         # Проверяем диапазон дат
-        date_from = self.form.cleaned_data.get('date_from')
-        date_to = self.form.cleaned_data.get('date_to')
+        date_from = self.form.cleaned_data.get("date_from")
+        date_to = self.form.cleaned_data.get("date_to")
 
         if date_from and date_to:
             if date_to < date_from:
@@ -31,3 +49,15 @@ class LessonFilter(filters.FilterSet):
         return super().filter_queryset(queryset)
 
 
+class GroupFilter(BaseFilterSetWithFormat):
+    faculty = filters.ModelChoiceFilter(
+        field_name="faculty",
+        queryset=Faculty.objects.filter(is_active=True),
+        to_field_name="id",
+        required=False,
+    )
+    grade = filters.NumberFilter(field_name="grade", required=False)
+
+    class Meta:
+        model = Group
+        fields = ["faculty", "grade", "format"]
