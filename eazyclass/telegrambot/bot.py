1a3bd58787771fe5630341eae77235388f1648d9
anton moiseev <moucee13@gmail.com>
2025-08-26 04:42:16 +0300

    Added UserContextMiddleware

--- eazyclass/telegrambot/bot.py
+++ eazyclass/telegrambot/bot.py
@@ -14,6 +14,7 @@ from telegrambot.handlers import (
     navigation_router,
     action_router,
 )
+from telegrambot.middleware import UserContextMiddleware
 from telegrambot.tasks import setup_periodic_task_scheduler
 
 logging.basicConfig(level=getattr(logging, settings.log_level), stream=sys.stdout)
@@ -24,7 +25,6 @@ logger = logging.getLogger(__name__)
 async def on_startup(deps: Container):
     api_client = deps.api_client()
     cache_service = deps.cache_service()
-    # await api_client.start()
     await cache_service.update_all()  # Первичное обновление клавиатур
     await setup_periodic_task_scheduler(deps=deps)  # Запуск планировщика
     logger.info("Bot started.")
@@ -43,6 +43,8 @@ async def main():
     bot = container.bot()
     storage = container.storage()
     dp = Dispatcher(bot=bot, storage=storage, deps=container)
+    dp.message.middleware(UserContextMiddleware())
+    dp.callback_query.middleware(UserContextMiddleware())
 
     dp.include_router(action_router)
     dp.include_router(start_router)
@@ -60,63 +62,3 @@ async def main():
 if __name__ == "__main__":
     asyncio.run(main())
 
-
-#
-# # Асинхронный обработчик нажатий на кнопки
-# @bot.callback_query_handler(func=lambda call: True)
-# async def handle_callback_query(call):
-#     try:
-#         chat_id = call.message.chat.id
-#         msg_id = call.message.message_id
-#         telegram_id = call.from_user.id
-#         user_data = User.objects.get_user_data_by_telegram_id(telegram_id)
-#         action, *params = call.data.split(':')
-#         if action == 'home':
-#             message, keyboard_key = generate_home_answer(user_data)
-#             await bot.edit_message_text(chat_id=chat_id, message_id=msg_id,
-#                                         text=message, reply_markup=get_keyboard(keyboard_key))
-#         # Выбор группы
-#         elif action == 'faculties':
-#             await bot.edit_message_text(chat_id=chat_id, message_id=msg_id,
-#                                         text="Выберите направление", reply_markup=get_keyboard(call.data))
-#
-#         elif action == 'faculty':
-#             await bot.edit_message_text(chat_id=chat_id, message_id=msg_id,
-#                                         text="Выберите курс", reply_markup=get_keyboard(call.data))
-#
-#         elif action == 'grade':
-#             await bot.edit_message_text(chat_id=chat_id, message_id=msg_id,
-#                                         text="Выберите группу", reply_markup=get_keyboard(call.data))
-#
-#         elif action == 'teachers':
-#             await bot.edit_message_text(chat_id=chat_id, message_id=msg_id,
-#                                         text="Выберите преподавателя", reply_markup=get_keyboard(call.data))
-#
-#         elif action == 'initial':
-#             await bot.edit_message_text(chat_id=chat_id, message_id=msg_id,
-#                                         text="Выберите преподавателя", reply_markup=get_keyboard(call.data))
-#
-#         elif action == 'context':
-#             context = context_data_store[call.data]
-#             CacheService.update_user_context(telegram_id, context)
-#             message = f'{context['title']}'
-#             await bot.edit_message_text(chat_id=chat_id, message_id=msg_id,
-#                                         text=message, reply_markup=get_keyboard('subscribe'))
-#
-#         elif action == 'subscribe':
-#             context = user_data['context']
-#             model_name = context['model']
-#             obj_id = context.get('id')
-#             user_id = user_data['user_id']
-#
-#             SubscriptionService.create_subscription(user_id, model_name, obj_id)
-#             CacheService.invalidate_user_cache(telegram_id)
-#
-#             call.data = 'home'
-#             await handle_callback_query(call)
-#
-#     except Exception as e:
-#         logger.error(f"Ошибка в обработке callback_query кнопки: {str(e)}")
-#         error_message = "Кажется что-то пошло не так. Попробуйте повторить позже"
-#         await bot.edit_message_text(chat_id=chat_id, message_id=msg_id,
-#                                     text=error_message, reply_markup=get_keyboard('start'))
--- eazyclass/telegrambot/middleware.py
+++ eazyclass/telegrambot/middleware.py
@@ -1,4 +1,7 @@
 from aiogram import BaseMiddleware
+from aiogram.types import TelegramObject
+from typing import Callable, Dict, Any, Awaitable
+from telegrambot.context import request_context
 
 from telegrambot.dependencies import Container
 
@@ -11,4 +14,23 @@ class DependencyMiddleware(BaseMiddleware):
     async def __call__(self, handler, event, data):
         # Передаём весь контейнер
         data["deps"] = self.container
+        return await handler(event, data)
+
+
+class UserContextMiddleware(BaseMiddleware):
+    async def __call__(
+        self,
+        handler: Callable[[TelegramObject, Dict[str, Any]], Awaitable[Any]],
+        event: TelegramObject,
+        data: Dict[str, Any],
+    ) -> Any:
+        user = getattr(event, "from_user", None)
+
+        if user:
+            # Ставим контекст пользователя
+            request_context.set({
+                "user_id": str(user.id),
+                "hmac": False,
+            })
+
         return await handler(event, data)
