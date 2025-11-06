76df6480f10d0ec45bc19b2a7a044c7b971990dc
anton moiseev <moucee13@gmail.com>
2025-11-06 22:13:20 +0300

    updated start lesson notification text (+ lesson date,  - subscription title)

--- eazyclass/scheduler/notifications/utils.py
+++ eazyclass/scheduler/notifications/utils.py
@@ -1,13 +1,30 @@
+from datetime import date as Date
+
 from scheduler.models import Lesson
+
+
+WEEKDAYS_RU = [
+    "Понедельник", "Вторник", "Среда",
+    "Четверг", "Пятница", "Суббота", "Воскресение"
+]
+
+def format_date_verbose(date: Date) -> str:
+    """Возвращает дату в формате '7 ноября (ПТ)'."""
+    weekday = WEEKDAYS_RU[date.weekday()]
+    return f"{weekday} {date.strftime("%d.%m.%Y")}"
 
 
 def format_group_lesson_message(lessons: list[Lesson]) -> str:
     if not lessons:
         return "Нет предстоящих уроков."
 
-    # заголовок
-    group_title = lessons[0].group.title
-    parts = ["Предстоящее занятие 📚 \n" f"группа <b>{group_title}</b>"]
+    # group_title = lessons[0].group.title
+    date_str = format_date_verbose(lessons[0].period.date)
+    parts = [
+        "Предстоящее занятие 🔔",
+        # f"группа <b>{group_title}</b>",
+        f"{date_str}"
+    ]
 
     # формируем блок для каждого урока
     for lesson in lessons:
@@ -31,16 +48,18 @@ def format_group_lesson_message(lessons: list[Lesson]) -> str:
 
 def format_teacher_lesson_message(lesson: Lesson) -> str:
     number = f"{lesson.period.lesson_number}\ufe0f\u20e3"
+    date_str = format_date_verbose(lesson.period.date)
     start_time = lesson.period.start_time.strftime("%H:%M")
     classroom = getattr(lesson.classroom, "title", "—")
     subject = getattr(lesson.subject, "title", "—")
-    teacher = getattr(lesson.teacher, "short_name", "—")
+    # teacher = getattr(lesson.teacher, "short_name", "—")
     group = getattr(lesson.group, "title", "—")
     subgroup = f"({lesson.subgroup} подгруппа)" if lesson.subgroup != "0" else ""
 
     return (
-        f"Предстоящее занятие 📚\n"
-        f"преподаватель <b>{teacher}</b>\n"
+        f"Предстоящее занятие 🔔 \n"
+        # f"преподаватель <b>{teacher}</b>\n"
+        f"{date_str}\n"
         "<blockquote>"
         f"{number} {start_time}  📍 {classroom}\n"
         f"<b>{subject}</b>\n"
