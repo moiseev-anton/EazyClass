6cfd838040a950c89073883cc126c981f818fd99
anton moiseev <moucee13@gmail.com>
2025-11-06 22:04:44 +0300

    switched from ETA notification tasks to "one_off" periodic tasks

--- eazyclass/scheduler/tasks/__init__.py
+++ eazyclass/scheduler/tasks/__init__.py
@@ -1,9 +1,6 @@
 from scheduler.tasks.group_scraper import update_groups
 from scheduler.tasks.run_schedule_spider import run_schedule_spider
-from scheduler.tasks.start_lesson_notifications import (
-    schedule_lesson_notifications,
-    send_lesson_notifications,
-)
+from scheduler.tasks.start_lesson_notifications import schedule_lesson_notifications, send_lesson_and_report
 from scheduler.tasks.sync_schedule import sync_schedule
 from scheduler.tasks.telegram_notification import send_telegram_notifications
 from scheduler.tasks.update_schedule import update_schedule_pipeline
