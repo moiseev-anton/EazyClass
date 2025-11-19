e02582f96a48a5e81c7e3f8b8fb21368a13a361d
anton moiseev <moucee13@gmail.com>
2025-11-19 06:14:20 +0300

    added get_date_map to PeriodManager

--- eazyclass/scheduler/managers/period_manager.py
+++ eazyclass/scheduler/managers/period_manager.py
@@ -1,4 +1,6 @@
 import logging
+from datetime import date
+from typing import Iterable
 
 from django.db import models
 from scheduler.managers.mixins import IDMappableMixin
@@ -7,4 +9,5 @@ logger = logging.getLogger(__name__)
 
 
 class PeriodManager(models.Manager, IDMappableMixin):
-    ...
+    def get_date_map(self, period_ids: Iterable[int]) -> dict[int, date]:
+        return dict(self.filter(id__in=period_ids).values_list("id", "date"))
