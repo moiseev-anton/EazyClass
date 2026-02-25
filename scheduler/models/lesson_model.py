f8fa8b0f8e851c4c1170f3875326c69cd26ddb2e
anton moiseev <moucee13@gmail.com>
2026-02-25 20:32:21 +0300

    added Lesson.teacher blank=True

--- scheduler/models/lesson_model.py
+++ scheduler/models/lesson_model.py
@@ -9,7 +9,7 @@ class Lesson(TimestampedModel):
     group = models.ForeignKey('scheduler.Group', related_name='lessons', on_delete=models.CASCADE, null=True)
     period = models.ForeignKey('scheduler.Period', related_name='lessons', on_delete=models.CASCADE, null=True)
     subject = models.ForeignKey('scheduler.Subject', related_name='lessons', on_delete=models.CASCADE, null=True)
-    teacher = models.ForeignKey('scheduler.Teacher', related_name='lessons', on_delete=models.CASCADE, null=True)
+    teacher = models.ForeignKey('scheduler.Teacher', related_name='lessons', on_delete=models.CASCADE, null=True, blank=True)
     classroom = models.ForeignKey('scheduler.Classroom', related_name='lessons', on_delete=models.CASCADE, null=True)
     subgroup = models.CharField(max_length=1, default='0')
     is_active = models.BooleanField(default=True)
