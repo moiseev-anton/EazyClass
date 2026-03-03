b184d9c66a3d01c1f3099a180685f9e7a9ae6a02
anton moiseev <moucee13@gmail.com>
2026-03-03 07:00:45 +0300

    Added guide template

--- scheduler/urls.py
+++ scheduler/urls.py
@@ -4,4 +4,5 @@ from django.views.generic import TemplateView
 urlpatterns = [
     path("terms/", TemplateView.as_view(template_name="legal/terms.html"), name="terms"),
     path("privacy/", TemplateView.as_view(template_name="legal/privacy.html"), name="privacy"),
+    path("guide/", TemplateView.as_view(template_name="legal/guide.html"), name="guide"),
 ]
