8f9842dd6efb2faa11b5961e470c833380d1da0c
anton moiseev <moucee13@gmail.com>
2026-04-27 21:52:57 +0300

    Add migrations

---
+++ scheduler/migrations/0005_rename_sub_slot_period_part_and_more.py
@@ -0,0 +1,9 @@
+from django.db import migrations
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ("scheduler", "0004_alter_period_unique_together_period_sub_slot_and_more"),
+    ]
+
+    operations = []
